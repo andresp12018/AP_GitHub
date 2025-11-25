606164a2b0fb9662913ea02e4b2052945402be4f
Andres Peralta <andresp1@gcartopel.com>
2025-11-25 15:04:13 -0500

    cambio

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 """
-Script simple que imprime Hola Mundo
+Servidor Flask con endpoint GET básico
 """
- 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hola_mundo():
+    return "hola mundo"
+
 if __name__ == "__main__":
-    print("Hola Mundo 2")
+    app.run(host='0.0.0.0', port=3000, debug=True)
